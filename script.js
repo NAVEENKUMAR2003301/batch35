7f6738a5aa501d314a12816f9af188dedc83dc53
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2026-06-18 17:40:11 +0530

    dataType , operators , concatination

--- script.js
+++ script.js
@@ -216,3 +216,355 @@ console.error(100);
 // 4. console.clear()
 
 console.clear()
+
+
+
+
+
+
+
+// DataTypes
+
+
+// primitive datatype 
+
+// 1. string - "hjbhkj"
+
+let a = "123"
+
+console.log(typeof(a));
+
+
+// 2. number - 123
+
+let b = 123
+
+console.log(typeof(b));
+
+
+// 3. boolean - true , false
+
+let c = true
+
+console.log(typeof(c));
+
+
+// 4. undefined 
+
+let d;
+
+console.log(d);
+
+
+// 5. null
+
+// let e = prompt()
+
+// console.log(e);
+
+
+// non primitive datatype
+
+// 1. array
+
+// syntex - []
+
+
+// example 
+
+let fruit = ["apple","orange","banana","graphs","guava","cherry","pineApple","pumbkin","RC car"]
+
+console.log(fruit);
+console.log(fruit[0]);
+console.log(fruit[2]);
+console.log(fruit[4]);
+
+
+console.log(fruit[fruit.length-1]);
+console.log(fruit[fruit.length-2]);
+
+
+
+// 2. object
+
+
+// syntex - {}
+
+
+let allThings = {
+    fruitVal : ["apple","orange","banana","graphs","guava",   "cherry","pineApple"],
+    vegVal   : "pumbkin",
+    playThing: "Rc Car"
+}
+
+
+console.log(allThings);
+console.log(allThings.vegVal);
+console.log(allThings.playThing);
+console.log(allThings.fruitVal[2]);
+
+
+console.clear()
+
+// operators
+
+
+// 1. Arithmetic operator
+
+// Addition     + 
+
+console.log(3 + 5);
+
+// subraction    - 
+
+console.log(2-1);
+
+// multiplication  *
+
+console.log(4*4);
+
+
+// division      / 
+
+
+console.log(5/5);
+
+
+// modulus      %
+
+console.log(5 % 5);
+
+
+// Exponencial   **
+
+console.log(5**3); // 5^3 -- 5*5*5 = 25
+
+
+// Increament     ++ = +1
+
+// pre Inc = ++var
+
+// post Inc = var++
+
+// Decreament     -- = -1
+
+// pre Dec = --var
+
+// Post Dec= var--
+
+
+
+let a1 = 5
+
+a1--
+
+console.log(a1);
+
+// your searching element first find in variable side means that time you can calculate value side only varible yet.
+
+
+let a2 = 2     // null
+
+    a2 = ++a2
+
+
+    console.log(a2);
+    
+
+
+let a3 = 1
+
+    a3 = a3--;
+
+    console.log(a3);
+
+
+
+
+
+
+
+
+
+
+// your searching element first find in value side means that time you can calculate  value side all value yet.
+
+
+
+let a4 = 3   // null
+
+let a5 = a4-- // a4 // 3
+
+console.log("a4 :",a4); // 2
+
+console.log("a5 :",a5); // 3
+
+
+
+let a6 = 5
+
+let a7 = --a6
+
+
+console.log("a6 :",a6); // 5   // --a6 = -1+5 = 4
+console.log("a7 :",a7); // 4   // --a6 = -1+5 = 4
+
+
+    
+
+
+
+
+// 2. Assignment operator
+
+console.log("Assignment Operator");
+
+let b1 = 10
+
+let addition = 100
+
+    // b1 = b1 + addition
+
+    b1 **= addition
+
+    console.log(b1);
+    
+
+
+// 3. Relational operator
+
+console.log("relational operator");
+
+// meaning      operator          example         result
+
+// lessThen       <                 5 < 3         false
+// greaterThen    >                 5 > 3         true
+// lessThenEq     <=                5 <= 5        true
+// GreaterThenEq  >=                12 >= 2       true
+// loosyTypeEq    ==                5 == "5"      true
+// loosyNotEq     !=                5 != "6"      true
+// strictlyTypeEq ===               5==="5"       false
+// strictlyNotEq  !==               5!=="5"       true
+
+// example :- 
+
+// 1. What is the output of `5 < 3`? // false
+
+// 2. What is the output of `10 > 2`? // true
+
+// 3. What is the output of `5 <= 5`? // true
+
+// 4. What is the output of `12 >= 20`? // false
+
+// 5. What is the output of `5 == "5"`? // true
+
+// 6. What is the difference between `==` and `===`? // value , value and types
+
+// 7. What is the output of `5 != "6"`? // true
+ 
+// 8. What is the output of `5 === "5"`? // false
+
+// 9. Which operator checks both value and data type? // strictly
+
+// 10. What is the output of `5 !== "5"`? // true
+
+// 11. What is the output of `7 >= 7`?  // true
+
+// 12. What is the output of `2 < 8`?   // true
+ 
+// 13. What is the output of `10 <= 5`?  // false
+
+// 14. Which operator is used for "Not Equal"? // loosy not , strictly not
+
+// 15. Which comparison returns `false`?  // not satisfied the condition that its return false.
+
+
+
+// 4. logical operator
+
+
+// AND  - &&
+
+// true && true && true = true 
+
+// false && true && true = false 
+
+// // OR   - ||
+
+// false || false || false = false 
+
+// false || false || true  = true
+
+// // NOT  - !
+
+// !(true) = false , !(false) = true
+
+
+
+// example :- 
+
+// 1. What is the output?
+console.log(5 > 3 && 10 > 5); // true && true = true
+
+// 2. What is the output?
+console.log(8 < 4 || 15 > 10); // false || true = true
+
+// 3. What is the output?
+console.log(!(20 > 10));  // !(true) = false
+
+// 4. What is the output?
+console.log(7 <= 7 && 9 != "9");  // true && false = false
+
+// 5. What is the output?
+console.log(5 === "5" || 12 >= 12); // false || true = true
+
+console.clear();
+
+
+// 5. ternary operator
+
+
+// condition ? statement1 : statement2
+
+
+5 < 7 ? console.log("condition true") : console.log("condition false");
+
+
+
+// concatination  +
+
+let d1 = "iron"
+let d2 = "man"
+
+// ironman
+
+console.log(d1+d2);
+
+// Template string
+
+let e1 = "spider"
+let e2 = "man"
+
+console.log(`${e1}${e2}`);
+
+
+// ternary operator
+
+let userName = false
+
+let pwd      = true 
+
+
+userName == pwd ? console.log("allow the home page") : console.log("your username and pwd wrong");
+
+
+
+
+
+
+
+
+
+// 3. function
+
+// 4. date
